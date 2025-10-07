072ac3d6abbc7997ef4da3127298b2f354b0becd
Andrea Peter <andrea.peter@ecorobotix.com>
2025-10-07 15:39:46 +0200

    Extend arrays test

--- arrays/script.bash
+++ arrays/script.bash
@@ -3,11 +3,15 @@
 # https://www.gnu.org/software/bash/manual/html_node/Arrays.html
 
 # Declaration
-# indexed array can be declared without `declare -a`
+# indexed array CAN be declared without `declare -a`
 declare -a week_days=(Mon Tue Wed Thu Fri)
 
-# Associative arrays must be declared with `declare -A`
-declare -A fs_paths=([proc]=/proc [sys]=/sys)
+# Associative arrays MUST be declared with `declare -A`
+declare -A fs_paths=(
+  [proc]=/proc
+  [sys]=/sys
+  [other-fs]=/other
+)
 
 # Insert elements
 week_days[5]=Sat
@@ -23,7 +27,7 @@ for n in {0..7}; do
   echo "Day $((n + 1)) of the week is ${week_days[${n}]}"
 done
 
-for fs in proc home; do
+for fs in proc home other-fs; do
   echo "Path of ${fs} is ${fs_paths[${fs}]}"
 done
 
@@ -35,3 +39,11 @@ done
 for path in "${fs_paths[@]}"; do
   echo "${path}"
 done
+
+# Value for non-existing key is empty
+non_existing_key=non_existing
+# This gives an "unbound variable" error with `set -u`
+value="${fs_paths[${non_existing_key}]}"
+if [ -z "${value}" ]; then
+  echo "Key \"${non_existing_key}\" does not exist"
+fi
